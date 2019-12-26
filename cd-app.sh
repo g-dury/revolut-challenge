d0e0738feb1439b0959cc723d3c66a2a1f965893
g-dury <4507947+g-dury@users.noreply.github.com>
2019-12-26 14:34:45 +0100

    🐛 Hotifxing security creds

--- cd-app.sh
+++ cd-app.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 gcloud --quiet config set project $PROJECT_NAME
